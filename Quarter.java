650d7a495965b2094a0e2862bac16bd591e71b2f
Eli Lambert <lambertwe@appstate.edu>
2021-07-27 16:16:31 -0400

    Added Coin subclasses and fixed usdMint switch statement

---
+++ Quarter.java
@@ -0,0 +1,6 @@
+public class Quarter extends Coin {
+    public Quarter(){}
+    public String toString() {
+        return "Smelting coin. Quarters are smelted with 91.67% Cu and 8.33% Ni.";
+    }
+}
--- usdMint.java
+++ usdMint.java
@@ -1,31 +1,24 @@
 public class usdMint extends Mint {
     public Coin makeCoin(double denomination) {
-        Coin coin;
-        switch (denomination) {
-            case 0.01:
-                coin = new Penny();
-                break;
-            case 0.05:
-                coin = new Nickel();
-                break;
-            case 0.1:
-                coin = new Dime();
-                break;
-            case 0.25:
-                coin = new Quarter();
-                break;
-            case 0.5:
-                coin = new halfDollar();
-                break;
-            case 1:
-                coin = new dollarCoin();
-                break;
-            default:
-                break;
+        Coin coin = new fakeCoin();
+        if (denomination == 0.01) {
+            coin = new Penny();
+        } else if (denomination == 0.05) {
+            coin = new Nickel();
+        } else if (denomination == 0.1) {
+            coin = new Dime();
+        } else if (denomination == 0.25) {
+            coin = new Quarter();
+        } else if (denomination == 0.5) {
+            coin = new HalfDollar();
+        } else if (denomination == 1) {
+            coin = new Dollar();
         }
+
+        System.out.println(coin);
         System.out.println("Inspecting coin");
         System.out.println("Smoothing coin");
         System.out.println("Buffing coin");
         return coin;
     }
-}:w
+}
