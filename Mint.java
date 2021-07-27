0f7e16386b897a11db70a4d4d19489b2360a3225
Jay Fenwick <fenwickjb@appstate.edu>
2021-07-27 13:42:07 -0400

    forgot to add two files before committing

---
+++ Mint.java
@@ -0,0 +1,22 @@
+
+// I need a Mint class to test it.
+
+// No specific specs for what attributes might be in a Mint.
+
+public abstract class Mint {
+    private String name;
+    private String location;
+
+    public Mint(String name, String location) {
+	this.name = name;
+	this.location = location;
+    }
+    public Mint() {
+	this("NoName", "NoLocation");
+    }
+
+    // I LOVE LOVE LOVE overriding this so println works!
+    public String toString() {
+	return "[Mint: name="+name+",location="+location+"]";
+    }
+}
