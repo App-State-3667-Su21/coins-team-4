a3a9c87173cb9d9e374c830e12b0944ddc0a1c23
lawsoncr <lawsoncr@appstate.edu>
2021-07-29 10:30:02 -0400

    added values to coins

--- Nickel.java
+++ Nickel.java
@@ -1,5 +1,7 @@
 public class Nickel extends Coin {
-    public Nickel(){}
+    public Nickel(){
+        super("USD", 0.05);
+    }
     public String toString() {
         return "Smelting coin. Nickels are smelted with 75% Cu and 25% Ni.";
     }
