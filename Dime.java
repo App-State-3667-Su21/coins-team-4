7b616665e2984cb1a7e38947c4f657785ae22cec
Jay Fenwick <fenwickjb@appstate.edu>
2021-07-29 12:25:15 -0400

    testing nickel and quarter

--- Dime.java
+++ Dime.java
@@ -1,4 +1,7 @@
 public class Dime extends Coin {
+    public Dime() {
+        super("USD", 0.10);
+    }
     public String toString() {
         return "Smelting coin. Dimes are smelted with 91.67% Cu and 8.33% Ni.";
     }
--- Quarter.java
+++ Quarter.java
@@ -1,5 +1,7 @@
 public class Quarter extends Coin {
-    public Quarter(){}
+    public Quarter(){
+        super("USD", 0.25);
+    }
     public String toString() {
         return "Smelting coin. Quarters are smelted with 91.67% Cu and 8.33% Ni.";
     }
